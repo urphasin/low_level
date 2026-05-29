f8c6020830d4529f078431a83b7d39170eff40be
Otito Amuga <urphasin@gmail.com>
2026-05-28 23:04:03 -0600

    push

--- competition/boiler.cpp
+++ competition/boiler.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 
 int main() {
-
-    unsigned __int128 x;
+    /*
+    unsigned __int128 is a compiler extension (mainly GCC/Clang), 
+    not a standard C++ integer type, 
+    so the standard stream operators (<<) were never overloaded for it.
+    */
+    unsigned __int128 x = 4;
 
     return 0;
 }
