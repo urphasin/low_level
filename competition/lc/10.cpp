6a4325d8bb46ad6a3c1bbd0dabdb9942b64e544b
Otito Amuga <urphasin@gmail.com>
2026-05-28 23:03:35 -0600

    push.cpp

--- competition/lc/10.cpp
+++ competition/lc/10.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 
 int main() {
-    unsigned __int128 x;
+    /*
+    unsigned __int128 is a compiler extension (mainly GCC/Clang), 
+    not a standard C++ integer type, 
+    so the standard stream operators (<<) were never overloaded for it.
+    */
+    unsigned __int128 x = 4;
 
+    std::cout << x << std::endl;
 
     return 0;
 }
